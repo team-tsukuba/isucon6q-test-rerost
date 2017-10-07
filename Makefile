7b2a3dfd0d1dd2ed19933e9026e9873a1932b57e
rerost <gmailii@yahoo.co.jp>
2017-10-07 19:06:15 +0900

    add makefile for goose and fix charset

--- Makefile
+++ Makefile
@@ -11,3 +11,12 @@ nginx-reload:
 deploy:
 	git pull
 	make system-reload
+
+db_up:
+	/home/isucon/go/bin/goose up
+
+db_down:
+	/home/isucon/go/bin/goose down
+
+db_status:
+	/home/isucon/go/bin/goose status
--- db/migrations/20171007183102_AddStarInIsuda.sql
+++ db/migrations/20171007183102_AddStarInIsuda.sql
@@ -2,8 +2,8 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE star (
-  keyword varchar(191) NOT NULL,
-  user_name varchar(191) NOT NULL,
+  keyword varchar(191) CHARSET utf8 NOT NULL,
+  user_name varchar(191) CHARSET utf8 NOT NULL,
   created_at datetime
 );
 
