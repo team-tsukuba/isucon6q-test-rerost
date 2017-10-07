7b9145f9e06e1d8122d062ecffb2447fe1d27951
rerost <gmailii@yahoo.co.jp>
2017-10-07 18:44:43 +0900

    add db

---
+++ db/migrations/20171007183102_AddStarInIsuda.sql
@@ -0,0 +1,13 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE star (
+  keyword varchar(191) NOT NULL,
+  user_name varchar(191) NOT NULL,
+  created_at datetime
+);
+
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE star;
